e0edad8e4acbb4221b7f5c4b6564be8e0745ef56
Periphanes <johnwon0723@gmail.com>
2023-06-28 16:11:01 +0900

    Random Seeding

--- train.py
+++ train.py
@@ -18,3 +18,21 @@ from data.data_preprocess import get_data_loader
 
 from sklearn.metrics import classification_report
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+np.random.seed(args.seed)
+random.seed(args.seed)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+if args.cpu or not torch.cuda.is_available():
+    device = torch.device('cpu')
+else:
+    device = torch.device('cuda')
+
+print("Device Used : ", device)
+args.device = device
+
